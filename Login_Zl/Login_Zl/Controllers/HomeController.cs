85ba364f8f4c495055db71d18f508899f020f02d
nanson1998 <nanson1998@gmail.com>
2020-09-30 16:33:32 +0700

    initial commit

---
+++ Login_Zl/Login_Zl/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Login_Zl.Models;
+using Microsoft.AspNetCore.Http;
+using MySqlX.XDevAPI.Common;
+using Login_Zl.MyClass;
+using System.Data;
+
+namespace Login_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ConDB conDB = new ConDB();
+
+        public IActionResult Index()
+        {
+            if (ChkLogin() == true)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            DataTable dt = conDB.GetData($"SELECT * FROM `account` WHERE username = '{username}'; ");
+            if (dt.Rows.Count > 0)
+            {
+                if (dt.Rows[0]["password"].ToString() == EncodeString.MD5HashCryptography(password))
+                {
+                    HttpContext.Session.SetString("login", "1");
+                    return View("Index");
+                }
+            }
+            return View();
+        }
+        private bool ChkLogin() 
+        {
+            bool result = false;
+            if (HttpContext.Session.GetString("login") != null)
+            {
+                if (HttpContext.Session.GetString("login") == "1")
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Login_Zl/Login_Zl/MyClass/ConDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Login_Zl.MyClass
+{
+    public class ConDB
+    {
+        private string ConString = "server=localhost;usename=root;password=1234;database=accountsdb;";
+        public MySqlConnection myConnection;
+        public ConDB()
+        {
+            myConnection = new MySqlConnection(ConString);
+            myConnection.Open();
+        }
+
+        public DataTable GetData(string _sqlCommand)
+        {
+            MySqlCommand command = new MySqlCommand(_sqlCommand, myConnection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+        public void ExecuteQuery(string _sqlCommand)
+        {
+            MySqlCommand command = new MySqlCommand("", myConnection);
+            command.CommandText = _sqlCommand;
+            command.CommandType = CommandType.Text;
+            command.Connection = myConnection;
+            command.ExecuteNonQuery();
+        }
+    }
+}
